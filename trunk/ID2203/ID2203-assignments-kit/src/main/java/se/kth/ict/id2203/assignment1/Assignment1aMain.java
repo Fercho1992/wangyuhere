0e180e6d6e2a8c71572af94365781539685e02b4
wangyuhere <wangyuhere@d95a48d6-0e68-11df-9450-0db692d89755>
2010-02-09 10:16:21 +0000

    Assignment1a

--- trunk/ID2203/ID2203-assignments-kit/src/main/java/se/kth/ict/id2203/assignment1/Assignment1aMain.java
+++ trunk/ID2203/ID2203-assignments-kit/src/main/java/se/kth/ict/id2203/assignment1/Assignment1aMain.java
@@ -9,6 +9,7 @@ import se.kth.ict.id2203.assignment1.applicatoin.Application1aInit;
 import se.kth.ict.id2203.pfd.PerfectFailureDetector;
 import se.kth.ict.id2203.pfd.PfdInit;
 import se.kth.ict.id2203.pfd.PfdLink;
+import se.kth.ict.id2203.pp2p.PerfectPointToPointLink;
 import se.kth.ict.id2203.pp2p.delay.DelayLink;
 import se.kth.ict.id2203.pp2p.delay.DelayLinkInit;
 import se.sics.kompics.Component;
@@ -31,24 +32,33 @@ public class Assignment1aMain extends ComponentDefinition {
 	private static int selfId;
 	private static String commandScript;
 	Topology topology = Topology.load(System.getProperty("topology"), selfId);
-	
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args
+	 *            the arguments
+	 */
 	public static void main(String[] args) {
 		selfId = Integer.parseInt(args[0]);
 		commandScript = args[1];
 
 		Kompics.createAndStart(Assignment1aMain.class);
 	}
-	
+
+	/**
+	 * Instantiates a new assignment0 group0.
+	 */
 	public Assignment1aMain() {
 		// create components
-		Component timer = create(JavaTimer.class);
+		Component time = create(JavaTimer.class);
 		Component network = create(MinaNetwork.class);
 		Component pp2p = create(DelayLink.class);
 		Component pfd = create(PerfectFailureDetector.class);
 		Component app = create(Application1a.class);
 
 		// handle possible faults in the components
-		subscribe(handleFault, timer.getControl());
+		subscribe(handleFault, time.getControl());
 		subscribe(handleFault, network.getControl());
 		subscribe(handleFault, pp2p.getControl());
 		subscribe(handleFault, pfd.getControl());
@@ -60,20 +70,24 @@ public class Assignment1aMain extends ComponentDefinition {
 
 		trigger(new MinaNetworkInit(self, 5), network.getControl());
 		trigger(new DelayLinkInit(topology), pp2p.getControl());
-		trigger(new PfdInit(topology, 5000, 1000), pfd.getControl());
+		trigger(new PfdInit(topology, 1000, 1000), pfd.getControl());
 		trigger(new Application1aInit(commandScript, neighborSet, self), app
 				.getControl());
 
 		// connect the components
-		connect(app.getNegative(PfdLink.class), pfd.getPositive(PfdLink.class));
-		connect(app.getNegative(Timer.class), timer.getPositive(Timer.class));
+		connect(app.getNegative(Timer.class), time.getPositive(Timer.class));
+		connect(app.getNegative(PfdLink.class), pfd
+				.getPositive(PfdLink.class));
 		
-		connect(pfd.getNegative(PfdLink.class), pp2p.getPositive(PfdLink.class));
-		connect(pfd.getNegative(Timer.class), timer.getPositive(Timer.class));
-
-		connect(pp2p.getNegative(Timer.class), timer.getPositive(Timer.class));
-		connect(pp2p.getNegative(Network.class), network.getPositive(Network.class));
-
+		connect(pfd.getNegative(PerfectPointToPointLink.class), pp2p
+				.getPositive(PerfectPointToPointLink.class));
+		connect(pfd.getNegative(Timer.class), time.getPositive(Timer.class));
+		
+		
+		connect(pp2p.getNegative(Timer.class), time.getPositive(Timer.class));
+		connect(pp2p.getNegative(Network.class), network
+				.getPositive(Network.class));
+	
 	}
 
 	Handler<Fault> handleFault = new Handler<Fault>() {
--- trunk/ID2203/ID2203-assignments-kit/src/main/java/se/kth/ict/id2203/pfd/PfdLink.java
+++ trunk/ID2203/ID2203-assignments-kit/src/main/java/se/kth/ict/id2203/pfd/PfdLink.java
@@ -1,11 +1,14 @@
 package se.kth.ict.id2203.pfd;
 
 
+import se.kth.ict.id2203.pp2p.Pp2pDeliver;
+import se.kth.ict.id2203.pp2p.Pp2pSend;
 import se.sics.kompics.PortType;
 
 public class PfdLink extends PortType {
 	{
 		positive(CrashEvent.class);
-		negative(null);
+		positive(Pp2pDeliver.class);
+		negative(Pp2pSend.class);
 	}
 }
