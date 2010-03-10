af04eb562c8d481a99fa480faaa3d4234704dc23
wangyuhere <wangyuhere@d95a48d6-0e68-11df-9450-0db692d89755>
2010-03-10 11:48:27 +0000

    yu wang

--- IV2009/eLUX/eLUXApplicationServer/src/java/com/elux/system/webcustsys/WebCustSysBean.java
+++ IV2009/eLUX/eLUXApplicationServer/src/java/com/elux/system/webcustsys/WebCustSysBean.java
@@ -4,24 +4,38 @@ import java.util.Vector;
 
 import com.elux.ado.order.OrderItem;
 import com.elux.ado.product.ProductCategory;
-import com.elux.manager.customermgr.CustomerMgrBean;
 import com.elux.manager.customermgr.ICustomerMgr;
 import com.elux.manager.ordermgr.IOrderMgr;
-import com.elux.manager.ordermgr.OrderMgrBean;
 import com.elux.manager.productmgr.IProductMgr;
 import com.elux.manager.productmgr.ProductInfo;
-import com.elux.manager.productmgr.ProductMgrBean;
 import com.elux.manager.productmgr.ShortProductInfo;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 @Remote(IWebCustSys.class)
 @Stateless(name = "IWebCustSys")
 public class WebCustSysBean implements IWebCustSys {
 
-	private IOrderMgr orderMgr = new OrderMgrBean();
-	private IProductMgr productMgr = new ProductMgrBean();
-	private ICustomerMgr customerMgr = new CustomerMgrBean();
+	private IOrderMgr orderMgr;
+	private ICustomerMgr customerMgr;
+	private IProductMgr productMgr;
+
+    public WebCustSysBean() throws NamingException {
+        Context ctx = new InitialContext();
+
+        ctx.addToEnvironment(Context.INITIAL_CONTEXT_FACTORY,
+                "org.jnp.interfaces.NamingContextFactory");
+        ctx.addToEnvironment(Context.PROVIDER_URL, "127.0.0.1:1099");
+        ctx.addToEnvironment("java.naming.factory.url.pkgs",
+                "org.jboss.naming:org.jnp.interfaces");
+
+        orderMgr = (IOrderMgr) ctx.lookup("IOrderMgr/remote");
+        customerMgr = (ICustomerMgr) ctx.lookup("ICustomerMgr/remote");
+        productMgr = (IProductMgr) ctx.lookup("IProductMgr/remote");
+    }
 
 	@Override
 	public double getDiscount(int cusID, int proCatID) {
